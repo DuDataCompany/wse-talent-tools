23476f8081b0b569f00a8ee949d5931bd46d84fc
Devan Anjelito <anjelitodevan@gmail.com>
2024-01-01 13:32:54 +0700

    finish age calc

--- pages/5_👴🏻_Age_Calculator.py
+++ pages/5_👴🏻_Age_Calculator.py
@@ -24,17 +24,32 @@ with col2:
         min_value=date(1950, 1, 1),
         max_value=date(2049, 12, 31),
     )
+toggle_date = st.toggle("Calculate age to certain age")
+if toggle_date:
+    age_to_calc = st.number_input("Insert **age**", value=None, placeholder="Age")
+
 
 if dob_date is not None and end_date is not None:
     if end_date <= dob_date:
         # start date must be <= end date
-        st.error("DOB must be after end date")
+        st.error("DOB must be before end date")
     else:
+        # calculate
         age = relativedelta(end_date, dob_date)
+        if toggle_date and age_to_calc:
+            dob_plus_date = dob_date + relativedelta(years=age_to_calc)
+            end_date_to_age_to_calc = relativedelta(dob_plus_date, end_date)
+
         # write the result
         st.subheader("Result")
         st.write(
             f"""
-            Age = **{age.years} years {age.months} months {age.days} days**.
+            Current age = **{age.years} years {age.months} months {age.days} days**.
         """
         )
+        if toggle_date and age_to_calc:
+            st.write(
+                f"""
+                End date to age {age_to_calc: .0f} = **{end_date_to_age_to_calc.years} years {end_date_to_age_to_calc.months} months {end_date_to_age_to_calc.days} days**.
+            """
+            )
