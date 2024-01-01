bc073ec438f6f9ca115211c572f8ae1a21c1f775
Devan Anjelito <anjelitodevan@gmail.com>
2024-01-01 13:14:28 +0700

    finish penalty calcualtor

--- pages/4_⚖️_Penalty_Calculator.py
+++ pages/4_⚖️_Penalty_Calculator.py
@@ -9,37 +9,43 @@ st.divider()
 # date selector
 col1, col2 = st.columns(2)
 with col1:
-    start_date = st.date_input(
+    contract_start_date = st.date_input(
         "Select **contract start date**", format="DD/MM/YYYY", value=None
     )
 with col2:
-    end_date = st.date_input(
+    contract_end_date = st.date_input(
         "Select **contract end date**", format="DD/MM/YYYY", value=None
     )
 
 last_working_date = st.date_input(
     "Select **last working date**", format="DD/MM/YYYY", value=None
 )
+base_salary = st.number_input(
+    "Insert **base salary**", value=None, placeholder="Base salary"
+)
 
 
-if start_date is not None and end_date is not None and last_working_date is not None:
-    if end_date <= start_date:
+if (
+    contract_start_date is not None
+    and contract_end_date is not None
+    and last_working_date is not None
+    and base_salary is not None
+):
+    if contract_end_date <= contract_start_date:
         # start date must be <= end date
-        st.error("Contracr end date must be after start date")
-#     else:
-#         diff = relativedelta(end_date, start_date)
-#         if diff.years >= 7:
-#             bonus = "**:green[3x base salary]**."
-#         elif diff.years >= 5:
-#             bonus = "**:green[2x base salary]**."
-#         else:
-#             bonus = "**:red[0]**."
+        st.error("Contract end date must be after start date")
+    else:
+        end_date_to_last_wd = relativedelta(contract_end_date, contract_start_date)
+        penalty = end_date_to_last_wd.months * base_salary
 
-#         # write the result
-#         st.subheader("Result")
-#         st.write(
-#             f"""
-#             Working period = **{diff.years} years {diff.months} months {diff.days} days**.
-#         """
-#         )
-#         st.write(f"Bonus = {bonus}")
+        # write the result
+        st.subheader("Result")
+        st.write(
+            f"""
+            Remaining contract = **{end_date_to_last_wd.years} years {end_date_to_last_wd.months} months {end_date_to_last_wd.days} days**.
+        """
+        )
+        st.write(f"Penalty = base salary * remaining contract")
+        st.write(
+            f"Penalty = {base_salary:,.0f} * {end_date_to_last_wd.months} = {base_salary * end_date_to_last_wd.months: ,.0f}"
+        )
