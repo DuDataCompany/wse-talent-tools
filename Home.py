af76f5a6892091b81f2899aa92c896cf602386f0
Devan Anjelito <anjelitodevan@gmail.com>
2024-06-10 20:04:26 +0700

    finish salary calc

--- Home.py
+++ Home.py
@@ -4,25 +4,33 @@ import streamlit as st
 def main():
     st.title("TO Tools and Calculator")
     st.write("Created : 26 Dec 2023")
-    st.write("Updated : 8 Jun 2024")
+    st.write("Updated : 10 Jun 2024")
     st.divider()
 
     st.write(
         """
-        This website provides tools to make employee calculations easy. 
-        Choose the tool you need from the left sidebar.
-    """
+        Welcome to the TO Tools and Calculator website. This platform provides various tools to simplify employee-related calculations.
+        Select the tool you need from the left sidebar.
+        """
     )
     st.write(
         """
-        Tools selection:
-        1. **📅 Days Calculator**: Find the number of days between two dates.
+        Available Tools:
+        1. **📅 Days Calculator**: Determine the number of days between two dates.
         2. **💰 Last Salary Hold or Pay**: Decide whether to hold or pay the salary of a resigning employee.
-        3. **🎉 Bonus Calculator**: Calculate the bonus received by an employee.
-        4. **⚖️ Penalty Calculator**: Calculate the penalty for a contract employee who resigns.
-        5. **👴🏻 Age Calculator**: Find the age from DOB.
-        6. **💸 SalaryCalculator**: Calculate THP given a base salary.
-    """
+        3. **🎉 Bonus Calculator**: Calculate the bonus an employee is entitled to receive.
+        4. **⚖️ Penalty Calculator**: Calculate the penalty for a contract employee who resigns early.
+        5. **👴🏻 Age Calculator**: Calculate age from the date of birth.
+        6. **💸 Salary Calculator**: Compute the take-home pay given a base salary.
+        """
+    )
+    st.divider()
+    st.write(
+        """
+        **Notes:**
+        1. Always double-check the calculations with your common sense to ensure accuracy.
+        2. If you encounter any errors, please notify Devan.
+        """
     )
 
 
--- pages/6_💸_Salary_Calculator.py
+++ pages/6_💸_Salary_Calculator.py
@@ -1,34 +1,82 @@
 import streamlit as st
+from src import salary_calculator
 
 st.header("Salary Calculator")
 st.write("Use this to find out THP given a base salary.")
 st.divider()
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.write("Input the information here:")
-    base_salary = st.number_input("Base salary", value=None, placeholder=0)
-    tax_status = st.selectbox(
-        "Tax/marriage status",
-        (
-            "TK/0",
-            "TK/1",
-            "TK/2",
-            "TK/3",
-            "K/0",
-            "K/1",
-            "K/2",
-            "K/3",
-        ),
-    )
-    insurance = st.number_input(
-        "Monthly insurance premium",
-        value=None,
-        placeholder=0,
-    )
-    bpjs_kes = st.checkbox("BPJS Kes")
-    bpjs_tk = st.checkbox("BPJS TK")
 
-with col2:
-    st.write("See the calcualtion here:")
+
+st.write("#### Input")
+base_salary = st.number_input("Base salary", value=None, placeholder=0)
+tax_status = st.selectbox(
+    "Tax/marriage status",
+    (
+        "TK/0",
+        "TK/1",
+        "TK/2",
+        "TK/3",
+        "K/0",
+        "K/1",
+        "K/2",
+        "K/3",
+    ),
+)
+insurance = st.number_input(
+    "Monthly insurance premium",
+    value=None,
+    placeholder=0,
+)
+bpjs_kes = st.checkbox("BPJS Kes")
+bpjs_tk = st.checkbox("BPJS TK")
+
+st.write("###")
+calculate = st.button("Calculate!", type="primary")
+
+
+if calculate:
+
+    st.divider()
+    st.write("#### Result")
+
+    # check if base salary is error
+    if not base_salary:
+        st.error("Base salary cannot be empty.")
+    elif int(base_salary) < 4_000_000:
+        st.error(f"Are you sure the base salary is only {base_salary:,}?")
+    else:
+        base_salary = int(base_salary)
+    
+        # if there is no insurance premium use 0
+        if not insurance:
+            insurance = 0
+
+        # calculate
+        result = salary_calculator.calculate_thp(base_salary, tax_status, insurance, bpjs_tk, bpjs_kes)
+
+        # print result
+        st.write(f"Base salary = **{base_salary:,}**")
+        st.write(f"Tax = **- {result.get("tax"):,}**")
+        st.write(f"JHT Employee = **- {result.get("jht_employee"):,}**")
+        st.write(f"JP Employee = **- {result.get("jp_employee"):,}**")
+        st.write(f"JKN Employee = **- {result.get("jkn_employee"):,}**")
+        st.write(f"Take Home Pay = **{result.get("thp"):,}**")
+
+        st.divider()
+        st.write("#### Note")
+        st.write(
+            """
+            1. Take-home pay is calculated as: Base Salary + Tax + JHT Employee + JP Employee + JKN Employee.
+            2. The Salary Calculator is a simulation tool and cannot be used as a legal reference. In case the actual salary differs from the calculations listed here, refer to official regulation.
+            """
+        )
+        st.divider()
+        st.write("#### Reference")
+        st.write(
+            """
+            1. [Government Regulation Number 58 of 2023](https://jdih.kemenkeu.go.id/in/dokumen/peraturan/067e1587-ae13-489a-4319-08dc0905f328).
+            1. [2024 Tax Calculation Example by Mekari](https://klikpajak.id/blog/pajak-penghasilan-pasal-21-2/).
+            """
+        )
+
